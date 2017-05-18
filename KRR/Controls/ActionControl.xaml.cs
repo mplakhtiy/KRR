39e62c2c1c831a01db29369892a681d67bc3f347
niunciusz <niunciusz@gmail.com>
2017-05-18 15:15:13 +0200

    GUI v2

---
+++ KRR/Controls/ActionControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KRR.Controls
+{
+    /// <summary>
+    /// Interaction logic for ActionControl.xaml
+    /// </summary>
+    public partial class ActionControl : UserControl
+    {
+        public ActionControl(Logic.Agent ag)
+        {
+            InitializeComponent();
+            foreach (var action in ag.Actions)
+            {
+                Entry entry = new Entry();
+                entry.AgentName.Content = action.Name;
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = GridLength.Auto;
+                ActionsGrid.RowDefinitions.Add(rowDefinition);
+
+                ActionsGrid.Children.Add(entry);
+            }
+        }
+    }
+}
--- KRR/MainWindow.xaml.cs
+++ KRR/MainWindow.xaml.cs
@@ -20,19 +20,20 @@ namespace KRR
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<Logic.Agent> agents = new List<Logic.Agent>();
+        public static Controls.Add add2;
+
         public MainWindow()
         {
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
-
-            Init();
-            
-            
+            Init();                      
         }
 
         public void Init()
         {
             Controls.Add add = new Controls.Add();
-            Controls.Add add2 = new Controls.Add();
+            add2 = new Controls.Add();
             Controls.Add add3 = new Controls.Add();
             Controls.Add2 add4 = new Controls.Add2();
 
@@ -44,15 +45,17 @@ namespace KRR
 
             add2.VerticalAlignment = VerticalAlignment.Bottom;
             add2.AddButton.Name = "Action";
+            //add2.Name.Name = "Action";
             Grid.SetRow(add2, 1);
             Grid.SetColumn(add2, 0);
             MainGrid.Children.Add(add2);
 
-            add3.VerticalAlignment = VerticalAlignment.Bottom;
+        
+            /*add3.VerticalAlignment = VerticalAlignment.Bottom;
             add3.AddButton.Name = "Statement";
             Grid.SetRow(add3,0);
             Grid.SetColumn(add3, 1);
-            MainGrid.Children.Add(add3);
+            MainGrid.Children.Add(add3);*/
 
             add4.VerticalAlignment = VerticalAlignment.Bottom;
             add4.AddButton.Name = "Fluent";
@@ -60,7 +63,40 @@ namespace KRR
             Grid.SetColumn(add4, 0);
             MainGrid.Children.Add(add4);
         }
-       
 
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            //new window
+            Windows.RuleWindow ruleWindow = new Windows.RuleWindow();
+            ruleWindow.ShowDialog();
+        }
+
+        public static void AddAgent(String agentName)
+        {
+            Logic.Agent agent = new Logic.Agent(agentName);
+            agents.Add(agent);
+
+            
+        }
+
+        private void AgentsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ActionsGrid.Children.Clear();
+            foreach (var ag in MainWindow.agents)
+            {
+                if (ag.Name.Equals(AgentsComboBox.SelectedValue.ToString()))
+                {
+                    RowDefinition rowDefinition = new RowDefinition();
+                    rowDefinition.Height = GridLength.Auto;
+                    AgentsGrid.RowDefinitions.Add(rowDefinition);
+
+                   // if (add2.Name.Name.Equals(null))
+                   // {
+                        Controls.ActionControl actioncontrol = new Controls.ActionControl(ag);
+                        ActionsGrid.Children.Add(actioncontrol);
+                  //  }
+                }
+            }
+        }
     }
 }
